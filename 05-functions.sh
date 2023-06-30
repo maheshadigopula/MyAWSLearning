3b1b076043b26572b5e41bb425769588b09083fa
Mahesh <amaheshbabu1999@gmail.com>
2023-06-30 11:40:19 +0530

    --Generic changes--

--- 05-functions.sh
+++ 05-functions.sh
@@ -11,7 +11,7 @@
 stat() {
     echo -e "Opened sessions: \e[33m$(who | wc -l)\e[0m"
     echo -e "Todays date: \e[34m$(date +%F)\e[0m"
-    echo -e "Load average in one minute: $(uptime | awk -F : '{print NF}' | awk -F , '{print $1}')"
+    echo -e "Load average in one minute: $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
 }
 
 stat

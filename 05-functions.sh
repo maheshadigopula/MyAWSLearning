bdbd46a02c3edca63f3f8a2b3bcc62013d598f92
Mahesh <amaheshbabu1999@gmail.com>
2023-06-30 11:39:37 +0530

    --Generic changes--

--- 05-functions.sh
+++ 05-functions.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-sample() {
-    echo -e "\e[35mI am in the function\e[0m"
+# sample() {
+#     echo -e "\e[35mI am in the function\e[0m"
+# }
+
+# echo "Calling function"
+
+# sample
+
+stat() {
+    echo -e "Opened sessions: \e[33m$(who | wc -l)\e[0m"
+    echo -e "Todays date: \e[34m$(date +%F)\e[0m"
+    echo -e "Load average in one minute: $(uptime | awk -F : '{print NF}' | awk -F , '{print $1}')"
 }
 
-echo "Calling function"
-
-sample
+stat

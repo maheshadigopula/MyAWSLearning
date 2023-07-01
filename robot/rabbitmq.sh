63eb78562f602e58f703b01a2f0994b6c0ecd544
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 22:42:26 +0530

    --Generic changes--

--- robot/rabbitmq.sh
+++ robot/rabbitmq.sh
@@ -7,12 +7,12 @@ echo -e "\e[33m______ $COMPONENT Configuration Started _________ \e[0m"
 
 echo -n "Installing and Configuring $COMPONENT :"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>> $LOGFILE
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-yum install rabbitmq-server -y
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> $LOGFILE
+yum install rabbitmq-server -y &>> $LOGFILE
 stat $?
 
 echo -n "Starting $COMPONENT :"
-systemctl enable rabbitmq-server 
+systemctl enable rabbitmq-server &>> $LOGFILE
 systemctl start rabbitmq-server
 stat $?
 

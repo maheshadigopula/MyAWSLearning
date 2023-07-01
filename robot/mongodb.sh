f32f296371a52d65cc5d89e6ea2d56b339417583
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 10:33:46 +0530

    --Generic changes--

--- robot/mongodb.sh
+++ robot/mongodb.sh
@@ -7,7 +7,7 @@ echo -n "Downloading ${COMPONENT} :"
 curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo &>> ${LOGFILE}
 stat $?
 
-echo -e -n "\eInstalling ${COMPONENT} :"
+echo -e -n "Installing ${COMPONENT} :"
 yum install -y mongodb-org &>> $LOGFILE
 stat $?
 

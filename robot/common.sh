3e56c8df967b6d747b13474e7312754f8cc8fe02
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 09:17:32 +0530

    --Generic changes--

--- robot/common.sh
+++ robot/common.sh
@@ -7,3 +7,11 @@ if [ ID -ne 0]; then
     echo "\e[31m You need to script either as a root user or with a sudo privilege \e[0m"
 fi
 
+stat() {
+
+    if [ $1 -eq 0 ]; then
+        echo -e "\e[32m Success \0m]"
+    else
+        echo -e "\e[31m Failure \e0m"
+    fi
+}
--- robot/frontend.sh
+++ robot/frontend.sh
@@ -5,24 +5,24 @@ COMPONENT=frontend
 
 source=common.sh
 
-echo -e "\e[33m------Installing nginx------\e[0m"
+echo -e "\e[33m----------Installing nginx----------\e[0m"
 
 echo -e -n "Installing nginx : "
 yum install nginx -y &>> /tmp/${COMPONENT}.log
 systemctl enable nginx &>> /tmp/${COMPONENT}.log
-
+stat $?
 
 echo -e -n "Starting nginx : "
 systemctl start nginx &>> /tmp/${COMPONENT}.log
 
 
-# curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/${COMPONENT}.zip
-# mv ${COMPONENT}-main/* .
-# mv static/* .
-# rm -rf ${COMPONENT}-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/${COMPONENT}.zip
+mv ${COMPONENT}-main/* .
+mv static/* .
+rm -rf ${COMPONENT}-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf

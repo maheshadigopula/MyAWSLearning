73003d7475c9c64eae7d2aa4d0d9476e146618d0
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 11:11:25 +0530

    --Generic changes--

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -12,9 +12,13 @@ curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>
 yum install nodejs -y &>> $LOGFILE
 stat $?
 
-echo -n "Creating Application User ${APPUSER} :"
-useradd ${APPUSER}  &>> $LOGFILE
-stat $?
+id ${APPUSER} &>> $LOGFILE
+if [ $? -ne 0 ]; then
+
+    echo -n "Creating Application User ${APPUSER} :"
+    useradd ${APPUSER}  &>> $LOGFILE
+    stat $?
+fi
 
 
 

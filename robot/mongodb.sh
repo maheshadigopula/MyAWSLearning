083767aadb81134e1428885ee0f55ed59a9cc462
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 10:30:05 +0530

    --Generic changes--

--- robot/mongodb.sh
+++ robot/mongodb.sh
@@ -5,7 +5,7 @@ LOGFILE=/tmp/${COMPONENT}.log
 
 source common.sh
 
-echo -n "Downloading ${COMPONENT}"
+echo -n "Downloading ${COMPONENT} : "
 curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo &>> ${LOGFILE}
 stat $?
 

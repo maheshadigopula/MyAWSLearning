40a72b8255b4dc78b66fc5579c1bca05b4a65aa2
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 10:32:23 +0530

    --Generic changes--

--- robot/mongodb.sh
+++ robot/mongodb.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 COMPONENT=mongodb
-
 source common.sh
 
-echo -n "Downloading ${COMPONENT} : "
+echo -n "Downloading ${COMPONENT} :"
 curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo &>> ${LOGFILE}
 stat $?
+
+echo -e -n "\eInstalling ${COMPONENT} :"
+yum install -y mongodb-org
+stat $?
+
+Echo -e -n "Starting ${COMPONENT} :"
+systemctl enable mongod
+systemctl start mongod
+stat $?
+
 

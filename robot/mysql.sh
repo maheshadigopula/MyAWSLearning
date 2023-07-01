7b34f0c21385a702303b52e731e8e2c3d685d52f
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 21:36:14 +0530

    --Generic changes--

--- robot/mysql.sh
+++ robot/mysql.sh
@@ -20,7 +20,7 @@ echo -n "Fetching the default password :"
 DEFAULT_ROOT_PWD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}') 
 stat $?
 
-echo show databases; | mysql -uroot -pRoboShop@1 &>> $LOGFILE
+echo show databases | mysql -uroot -pRoboShop@1 &>> $LOGFILE
 if [ $? -ne 0 ]; then
     echo -n "Resetting the default root password :"
     echo "ALTER USER 'root'@'localhost' IDENTIFIED By 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD} &>> $LOGFILE

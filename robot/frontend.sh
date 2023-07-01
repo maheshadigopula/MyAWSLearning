45ce61b24cf843039b566e9afde37e54a0863616
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 09:17:57 +0530

    --Generic changes--

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -16,13 +16,13 @@ echo -e -n "Starting nginx : "
 systemctl start nginx &>> /tmp/${COMPONENT}.log
 
 
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+# curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 
 
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/${COMPONENT}.zip
-mv ${COMPONENT}-main/* .
-mv static/* .
-rm -rf ${COMPONENT}-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/${COMPONENT}.zip
+# mv ${COMPONENT}-main/* .
+# mv static/* .
+# rm -rf ${COMPONENT}-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf

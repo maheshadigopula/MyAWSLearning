f1ec66d60a372c7881d84282e349cabf20a38cdd
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 09:14:17 +0530

    --Generic changes--

--- robot/common.sh
+++ robot/common.sh
@@ -1,2 +1,9 @@
 APPUSER=roboshop
 LOFGILE="/tmp/$COMPONENT.log"
+
+ID=$(id -u)
+
+if [ ID -ne 0]; then
+    echo "\e[31m You need to script either as a root user or with a sudo privilege \e[0m"
+fi
+

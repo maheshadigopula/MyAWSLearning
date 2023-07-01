abd72a112368d50e9018d957b57ac812f0281da8
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 15:06:09 +0530

    --Generic changes--

--- robot/redis.sh
+++ robot/redis.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 COMPONENT=redis
-# source robot/common.sh
+source robot/common.sh
 
 echo -e "\e[33m______ $COMPONENT Configuration Started _________ \e[0m"
 
 echo -n "Configuring the ${COMPONENT} Repo :"
-# curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> ${LOGFILE}
-# stat $?
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> ${LOGFILE}
+stat $?
 
-# echo -n "Installing ${COMPONENT} :"
-# yum install redis-6.2.11 -y &>> ${LOGFILE}
-# stat $?
+echo -n "Installing ${COMPONENT} :"
+yum install redis-6.2.11 -y &>> ${LOGFILE}
+stat $?
 
 

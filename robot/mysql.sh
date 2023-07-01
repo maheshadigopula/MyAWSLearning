0514aaf999f876d48a5c94d2978b150e324ccb45
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 21:53:09 +0530

    --Generic changes--

--- robot/mysql.sh
+++ robot/mysql.sh
@@ -27,8 +27,13 @@ if [ $? -ne 0 ]; then
     stat $?
 fi
 
-echo -n "Uninstalling Password Validate plugin :"
-echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1 &>> $LOGFILE
-stat $?
+echo "show plugins;"" | mysql -uroot -rRoboShop@1 | grep validate_password
+if [ $? -eq 0 ]; then
+    echo -n "Uninstalling Password Validate plugin :"
+    echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1 &>> $LOGFILE
+    stat $?
+fi
+
+
 
 

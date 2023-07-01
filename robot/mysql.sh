ce9ed797f8f81b728dca4ad774011faacb4e5824
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 21:08:56 +0530

    --Generic changes--

--- robot/mysql.sh
+++ robot/mysql.sh
@@ -4,15 +4,15 @@ COMPONENT=mysql
 source robot/common.sh
 
 echo -n "Downloading the ${COMPONENT} Repo :"
-curl -s -L -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mysql.repo &>> $LOGFILE
 stat $?
 
 echo -n "Installing ${COMPONENT} :"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>> $LOGFILE
 stat $?
 
 echo -n "Starting ${COMPONENT} :"
-systemctl enable mysqld 
-systemctl start mysqld
+systemctl enable mysqld &>> $LOGFILE
+systemctl start mysqld &>> $LOGFILE
 stat $?
 

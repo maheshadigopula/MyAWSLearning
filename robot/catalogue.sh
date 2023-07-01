2efc549872bb75ce759f53fd215d59af4c64bcf1
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 11:05:38 +0530

    --Generic changes--

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -4,7 +4,7 @@ COMPONENT=catalogue
 sourche robot/common.sh
 
 echo -n "Coonfiguring NodeJS Repo :"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
 yum install nodejs -y &>> $LOGFILE
 stat $?
 

0279b6e54a445ebd9210c5b8e16a8a4d350ceb50
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 11:05:58 +0530

    --Generic changes--

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 COMPONENT=catalogue
-sourche robot/common.sh
+source robot/common.sh
 
 echo -n "Coonfiguring NodeJS Repo :"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE

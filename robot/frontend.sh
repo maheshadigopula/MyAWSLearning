0f7a3edb4989e7a67e0b3883af73ee56a4c56171
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 09:39:12 +0530

    --Generic changes--

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 COMPONENT=frontend
+LOGFILE=/tmp/${COMPONENT}.log
 
 stat() {
     if [ $1 -eq 0 ]; then 
@@ -16,25 +17,32 @@ if [ $ID -ne 0 ]; then
     exit 1
 fi
 
-
 echo -e "\e[33m----------Installing nginx----------\e[0m"
 echo -e -n "Installing nginx : "
-yum install nginx -y &>> /tmp/${COMPONENT}.log
-systemctl enable nginx &>> /tmp/${COMPONENT}.log
+yum install nginx -y &>> $LOGFILE
+systemctl enable nginx &>> $LOGFILE
 stat $?
 
 echo -e -n "Starting nginx : "
-systemctl start nginx &>> /tmp/${COMPONENT}.log
+systemctl start nginx &>> $LOGFILE
 stat $?
 
+echo -e -n "Downloading the ${COMPONENT} : "
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
 
-# curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+echo -e -n "Clearing the default content : "
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
 
+echo -e -n "Extracting the ${COMPONENT} : "
+unzip /tmp/${COMPONENT}.zip &>> $LOGFILE
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/${COMPONENT}.zip
-# mv ${COMPONENT}-main/* .
-# mv static/* .
-# rm -rf ${COMPONENT}-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -e -n "Copying the ${COMPONENT} : "
+mv ${COMPONENT}-main/* . &>> $LOGFILE
+mv static/* . &>> $LOGFILE
+rm -rf ${COMPONENT}-main README.md &>> $LOGFILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
+stat $?
+

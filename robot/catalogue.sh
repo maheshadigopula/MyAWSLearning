7b620d8959a989d0bb03321d854a4a929ae9b974
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-01 14:09:20 +0530

    --Generic changes--

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -40,8 +40,8 @@ npm install &>> ${LOGFILE}
 stat $?
 
 echo -n "Configuring ${COMPONENT} Dependencied :"
-sed -i -e 's/MONGO_DNSNAME/172.31.0.40/' /home/${APPUSER}/${COMPONENT}/systemd.service
 mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service $>> ${LOGFILE}
+sed -i -e 's/MONGO_DNSNAME/172.31.0.40/' /etc/systemd/system/catalogue.service
 stat $?
 
 

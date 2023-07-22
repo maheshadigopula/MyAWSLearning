feb71fbcb5aec6a496eb0b39bd95b0417f604e1e
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-22 21:59:36 +0530

    --Generic changes--

--- application/robot/common.sh
+++ application/robot/common.sh
@@ -4,9 +4,9 @@ LOGFILE="/tmp/$COMPONENT.log"
 stat() {
 
     if [ $1 -eq 0 ]; then
-        echo -e "\e[32mSuccess[0m"
+        echo -e "\e[32mSuccess\e[0m"
     else
-        echo -e "\e[32mFailure[0m"
+        echo -e "\e[32mFailure\e[0m"
     fi
 }
 

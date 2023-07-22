78ccad036e8f15f067b34780c5488c5422567b4f
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-22 22:19:26 +0530

    --Generic changes--

--- application/robot/mongodb.sh
+++ application/robot/mongodb.sh
@@ -16,7 +16,7 @@ systemctl start mongod &>> $LOGFILE
 stat $?
 
 echo -n "whitelisting the ${COMPONENT} :"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>> $LOGFILE
 stat $?
 
 echo -n "Restarting the ${COMPONENT} :"
@@ -24,19 +24,19 @@ systemctl restart mongod
 stat $?
 
 echo -n "Downloading the ${COMPONENT} schema :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>> $LOGFILE
 stat $?
 
 
 echo -n "Extracting the ${COMPONENT} schema file :"
 cd /tmp
-unzip $COMPONENT.zip
+unzip $COMPONENT.zip &>> $LOGFILE
 stat $?
 
 echo -n "Injecting the schema :"
 cd $COMPONENT-main
-mongo < catalogue.js
-mongo < users.js
-stat $?
+mongo < catalogue.js &>> $LOGFILE
+mongo < users.js &>> $LOGFILE
+stat $? 
 
 echo -e "\e[33m---------------------------- $COMPONENT Configuration Completed -------------------------- \e[0m"

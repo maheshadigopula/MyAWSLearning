64e4f7347c8be813cd223a8ada4256c9db14466f
maheshadigopula <amaheshbabu1999@gmail.com>
2023-07-22 21:58:47 +0530

    --Generic changes--

--- application/robot/common.sh
+++ application/robot/common.sh
@@ -6,7 +6,7 @@ stat() {
     if [ $1 -eq 0 ]; then
         echo -e "\e[32mSuccess[0m"
     else
-        echo -e "\e[32mFailure[0m]"
+        echo -e "\e[32mFailure[0m"
     fi
 }
 
--- application/robot/frontend.sh
+++ application/robot/frontend.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-COMPONENT= frontend
+COMPONENT=frontend
 source common.sh
 
 echo -e "\e[33m-------------------$COMPONENT Configuration started--------------------"
 
-echo -n -e "Installing Nginx: "
+echo -n "Installing Nginx: "
 yum install nginx -y  &>> $LOGFILE
 systemctl enable nginx &>> $LOGFILE
 systemctl start nginx &>> $LOGFILE
 stat $?
+
+echo -n "Extracting the $COMPONENT: "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf

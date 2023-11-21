51a66effe9ad3bee3863d680e46b691773908565
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-21 16:14:57 +0530

    Updated code

--- robo/catalogue.sh
+++ robo/catalogue.sh
@@ -2,6 +2,7 @@
 
 source robo/common.sh
 COMPONENT=catalogue
+APPUSER=roboshop
 
 echo -e "\e[31m_________Configuration started________\e[0m"
 
@@ -9,6 +10,13 @@ echo -n "Configuring NodeJS Repo:"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>$Logfile
 yum install nodejs -y &>>$Logfile
 stat $?
+
+id $APPUSER
+if [ $? -ne 0 ] ; then
+    echo -e "Creating user $APPUSER:"
+    useradd $APPUSER &>>$Logfile
+    stat $?
+fi
 
 
 

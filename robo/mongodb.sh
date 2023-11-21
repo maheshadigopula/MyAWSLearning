8d835512b8ac62eccd0ab107d6c42a31a9f49aed
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-21 15:08:33 +0530

    Updated code

---
+++ robo/mongodb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source robo/common.sh
+
+COMPONENT=mongodb
+
+
+echo -e "\e[32m________Configuration Started________\e[0m"
+
+echo -n "\e[32mDownloading $COMPONENT:\e[0m"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+
+echo -n "\e[32mInstalling $COMPONENT:\e[0m"
+yum install -y mongodb-org
+stat $?
+
+echo -n "e[32mStarting $COMPONENT:\e[0m"
+systemctl enable mongod
+systemctl start mongod
+stat $?
+
+
+

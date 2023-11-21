f5fa098f06551ca3210d41de0009fa416e7b79ce
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-21 17:07:09 +0530

    Updated code

--- robo/catalogue.sh
+++ robo/catalogue.sh
@@ -18,6 +18,38 @@ if [ $? -ne 0 ] ; then
     stat $?
 fi
 
+echo -n "Downloading the ${COMPONENT} :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>>$Logfile
+stat $?
+
+echo -n "Cleaning and Extarcting ${COMPONENT} :"
+rm -rf /home/$APPUSER/$COMPONENT/
+cd /home/$APPUSER
+unzip -o /tmp/$COMPONENT.zip &>>$Logfile
+stat $?
+
+echo -n "Changing the ownership to ${APPUSER} :" 
+mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
+chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
+stat $?
+
+echo -n "Installing $COMPONENT Dependencies:"
+cd /home/roboshop/catalogue
+npm install
+stat $?
+
+echo -n "Configuring ${COMPONENT} Dependencies :"
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service &>>$Logfile
+sed -i -e 's/MONGO_DNSNAME/172.31.0.209' /etc/systemd/system/${COMPONENT}.service &>>$Logfile
+stat &?
+
+echo -n "Restarting $COMPONENT services:"
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
+systemctl status catalogue -l
+stat $?
+
 
 
 

59ad3d5a1676baef0470f95a3f0b21f201df175a
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-21 15:00:14 +0530

    Updated code

--- robo/frontend.sh
+++ robo/frontend.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-set -e
+source robo/common.sh
 
 COMPONENT=frontend
-Logfile="/tmp/$COMPONENT.log"
-
-ID=$(id -u)
-if [ $ID -ne 0 ] ; then
-    echo -e "\e[33m You need to run as root user or use Sudo\e[0m"
-    exit 1
-fi
-
-
-stat()
-{
-    if [ $1 -eq 0 ]; then
-        echo -e "\e[32m Success\e[0m"
-    else
-        echo -e "\e[33m Failure\e[0m"
-    fi
-}
-
-
 
 echo -e "\e[32m________$COMPONENT Configuration Started_________\e[0m"
 

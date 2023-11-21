70f82584fa376257db5f9b34ac2a00c8fc905e9d
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-21 12:28:07 +0530

    Updated code

--- robo/frontend.sh
+++ robo/frontend.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-COMPONENT=frontent
+set -e
+
+COMPONENT=frontend
+Logfile= "/tmp/#COMPONENT.log"
+
+stat()
+{
+    if [ $1 -eq 0 ]; then
+        echo -e "\e[32m Success\e[0m"
+    else
+        echo -e "\e[33m Failure\e[0m"
+    fi
+}
+
+
 
 echo -e "\e[32m________$COMPONENT Configuration Started_________\e[0m"
 
-
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
+echo -n "Installing Ngnix: "
+yum install nginx -y &>> Logfile
+stat $?

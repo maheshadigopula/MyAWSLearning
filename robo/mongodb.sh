30c7454607037ab157bb2713a6615651e55a647e
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-21 15:18:24 +0530

    Updated code

--- robo/mongodb.sh
+++ robo/mongodb.sh
@@ -7,21 +7,21 @@ COMPONENT=mongodb
 
 echo -e "\e[32m________Configuration Started________\e[0m"
 
-echo -n "\e[32mDownloading $COMPONENT:\e[0m"
+echo -n -e "\e[32mDownloading $COMPONENT:\e[0m"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
 
 
-echo -n "\e[32mInstalling $COMPONENT:\e[0m"
+echo -n -e "\e[32mInstalling $COMPONENT:\e[0m"
 yum install -y mongodb-org &>>$Logfile
 stat $?
 
-echo -n "e[32mStarting $COMPONENT:\e[0m"
+echo -n -e "e[32mStarting $COMPONENT:\e[0m"
 systemctl enable mongod &>>$Logfile
 systemctl start mongod &>>$Logfile
 stat $?
 
-echo -n "whitelisting the ${COMPONENT} :"
+echo -n -e "whitelisting the ${COMPONENT} :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 

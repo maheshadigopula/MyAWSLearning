296b697bc021ee28348f2d414a8d2559edf33a3c
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-21 15:40:33 +0530

    Updated code

---
+++ robo/catalogue.sh
@@ -0,0 +1,16 @@
+#!bin/bash/
+
+source robo/common.sh
+COMPONENT=catalogue
+
+echo -e "\e[31m_________Configuration started________\e[0m"
+
+echo -n "Configuring NodeJS Repo:"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install nodejs -y &>>$Logfile
+stat $?
+
+
+
+
+

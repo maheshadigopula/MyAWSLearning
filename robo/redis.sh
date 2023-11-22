253739240b2096d4fad0b4ffab98895af16f39eb
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-22 15:48:00 +0530

    Updated code

--- robo/redis.sh
+++ robo/redis.sh
@@ -7,7 +7,7 @@ echo -e "\e[31m_________Configuration started_________\e[0m"
 
 
 echo -n "Configuring the $COMPONENT Repo:"
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>$Logfile
+curl -L https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo -o /etc/yum.repos.d/$COMPONENT.repo &>>$Logfile
 stat $?
 
 echo -n "Installing $COMPONENT:"
@@ -15,8 +15,8 @@ yum install redis-6.2.13 -y &>>&Logfile
 stat $?
 
 echo -n "whitelisting the $COMPONENT"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$COMPONENT.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$COMPONENT/$COMPONENT.conf
 stat $?
 
 echo -n "Starting $COMPONENT : "

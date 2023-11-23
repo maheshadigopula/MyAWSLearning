a3a8e36df9d826cee4c42703e1b26804c6138272
maheshadigopula <amaheshbabu1999@gmail.com>
2023-11-23 21:28:29 +0530

    Updated code

---
+++ robo/cart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+source robo/common.sh
+COMPONENT=cart
+
+echo -e "\e[33m_________________Configuration started_________________\e[0m"
+
+NODEJS
+Creating_User
+Downloading_And_Extracting
+NPM_Install
+
+echo -e "\e[33m_____________Configuration Completed_______________\e[0m"

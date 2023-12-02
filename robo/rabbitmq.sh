fc11f637d3f4d587db0fe0d1fefcb6c30de5d9fe
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-02 17:56:56 +0530

    Updated code

--- robo/rabbitmq.sh
+++ robo/rabbitmq.sh
@@ -1,35 +1,34 @@
-#!/bin/bash/
+#!/bin/bash 
 
-source robo/common.sh
 COMPONENT=rabbitmq
+source robo/common.sh
 
-echo -e "\e[33m_____________Configuration Started______________\e[0m"
+echo -n "Installing and configuring $COMPONENT repo"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash  &>>Logfile
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>Logfile
+stat $? 
 
-echo -n "Downloading $COMPONENT dependencies:"
-curl -s https://packagecloud.io/install/repositories/$COMPONENT/erlang/script.rpm.sh | sudo bash &>>$Logfile
-curl -s https://packagecloud.io/install/repositories/$COMPONENT/$COMPONENT-server/script.rpm.sh | sudo bash &>>$Logfile
+echo -n "Installing $COMPONENT : "
+yum install rabbitmq-server -y &>>Logfile
 stat $?
 
-echo -n "Installing $COMPONENT:"
-yum install rabbitmq-server -y &>>$Logfile
-stat $?
+echo -n "Starting $COMPONENT :"
+systemctl enable rabbitmq-server &>>Logfile
+systemctl start rabbitmq-server  &>>Logfile
+stat $? 
 
-echo -n "Starting $COMPONENT:"
-systemctl enable rabbitmq-server 
-systemctl start rabbitmq-server
-stat $?
-
-sudo rabbitmqctl list_users | grep "${roboshop}" &>>$Logfile
-if [ $? -ne 0 ]; then
+sudo rabbitmqctl list_users | grep "${APPUSER}" &>>Logfile 
+if [ $? -ne 0 ]; then 
     echo -n "Creating Applicaiton user on $COMPONENT: "
-    rabbitmqctl add_user roboshop roboshop123 &>>$Logfile
-    stat $?
-fi
+    rabbitmqctl add_user roboshop roboshop123 &>>Logfile
+    stat $? 
+fi 
 
-echo -n "Adding permissions to $APPUSER:"
-rabbitmqctl set_user_tags roboshop administrator &>>$Logfile
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$Logfile
+
+echo -n "Adding Permissions to $APPUSER :"
+rabbitmqctl set_user_tags roboshop administrator &>>Logfile
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>Logfile
 stat $?
 
-echo -e "\e[33m __________ $COMPONENT Installation Completed _________ \e[0m"
 
+echo -e "\e[32m __________ $COMPONENT Installation Completed _________ \e[0m"

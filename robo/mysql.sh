b982392e116c3a1d752773a89e3641435c5806db
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-02 17:36:46 +0530

    Updated code

--- robo/mysql.sh
+++ robo/mysql.sh
@@ -2,7 +2,7 @@
 
 COMPONENT=mysql
 
-source components/common.sh    # Source loads a file and this file has all the common patterns.
+source robo/common.sh    # Source loads a file and this file has all the common patterns.
 
 echo -n "Configuring the $COMPONENT repo  : "
 curl -s -L -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo

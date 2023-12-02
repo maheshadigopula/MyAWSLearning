85232437453ca6e7aba9bdf94993b4e19b036e3b
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-02 18:13:49 +0530

    Updated code

--- launch-ec2.sh
+++ launch-ec2.sh
@@ -12,7 +12,7 @@ fi
 
 # AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
 AMI_ID="ami-0f75a13ad2e340a58"
-SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=My_Batch_52 --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
 
 echo -e "AMI ID Used to launch the instance is \e[32m $AMI_ID \e[0m "
 echo -e "Security Group ID Used to launch the instance is \e[32m  $SG_ID \e[0m"

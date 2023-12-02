bcd80732e36ba500153e03bc73bb3bb243a4bd9e
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-02 17:29:35 +0530

    Updated

--- robot/mysql.sh
+++ robot/mysql.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
 COMPONENT=mysql
-source robot/common.sh
+
+APPUSER=roboshop
+LOGFILE="/tmp/$COMPONENT.log"
+
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m "
+    else 
+        echo -e "\e[31m failure \e[0m"
+    fi 
+}
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+    echo -e "\e[31mYou need to script either as a root user or with a sudo privilege \e[0m"
+    exit 1
+fi
+
+
 echo -e "\e[33m______ $COMPONENT Configuration Started _________ \e[0m"
 
 echo -n "Downloading the ${COMPONENT} Repo :"

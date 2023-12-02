5f6e3c57270a5953c6dcdc2f3082dee323592bc5
maheshadigopula <amaheshbabu1999@gmail.com>
2023-12-02 17:52:32 +0530

    Updated code

--- robo/mysql.sh
+++ robo/mysql.sh
@@ -29,7 +29,7 @@ if [ $? -ne 0 ]; then
     stat $?
 fi 
 
-echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password; &>>Logfile
+echo "show plugins;"| mysql -uroot -pRoboShop@1 | grep validate_password; &>>Logfile
 if [ $? -eq 0 ]; then 
     echo -n "Uninstalling Password Validate Plugin " 
     echo "uninstall plugin validate_password;"| mysql -uroot -pRoboShop@1 &>>Logfile
